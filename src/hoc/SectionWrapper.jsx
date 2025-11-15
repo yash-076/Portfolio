ae0cae3d7997ebd98f1572b4ad01f054fef0b6ca
Yash Rastogi <smediayash@gmail.com>
2025-11-15 13:12:39 +0530

    Initial commit

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles.js'
+import { staggerContainer } from '../utils/motion'
+
+const SectionWrapper = (Component, idName) => {
+  const HOC = (props) => {
+    console.log('SectionWrapper render', idName) // debug
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        id={idName}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className='hash-span' id={idName}> &nbsp; </span>
+        <Component {...props} />
+      </motion.section>
+  )
+}
+
+  return HOC
+}
+
+export default SectionWrapper
